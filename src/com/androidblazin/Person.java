27e1c925f024d064be4ef201b448b72a050bb89d
shir60bhushan <shir60bhushan@gmail.com>
2014-10-02 18:16:17 +0530

    ORM example

---
+++ src/com/androidblazin/Person.java
@@ -0,0 +1,28 @@
+package com.androidblazin;
+
+import com.j256.ormlite.field.DatabaseField;
+
+public class Person {
+
+	@DatabaseField(generatedId = true)
+	private int id;
+	@DatabaseField
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String firstname) {
+		this.name = firstname;
+	}
+
+}
